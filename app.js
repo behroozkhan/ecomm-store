8c967eb8fc84b934f55852bbfd5c79342c89f0a5
Behrooz Khan <117090048+behroozkhan@users.noreply.github.com>
2023-07-24 18:27:36 +0500

    onAuthStateChanged add into main file app.js

--- app.js
+++ app.js
@@ -48,6 +48,49 @@
 //         }
 //     });
 // });
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import {
+  getAuth,onAuthStateChanged
+}  from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import {
+    getFirestore
+  } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyAOt0YRqCBI5MvtYzZaJiILrjlsA9vWn6w",
+    authDomain: "signup-login-74d21.firebaseapp.com",
+    projectId: "signup-login-74d21",
+    storageBucket: "signup-login-74d21.appspot.com",
+    messagingSenderId: "167744399342",
+    appId: "1:167744399342:web:50d079ab744413004a90ff",
+    measurementId: "G-PQXQJB56F5",
+  };
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+ onAuthStateChanged(auth, (user) => {
+    if (user && location.pathname !== "./user profile/profile.html") {
+      const uid = user.uid;
+      location.href = "./user profile/profile.html";
+      // location.href = '../login-register/login.js'
+      console.log("uid==>",uid);
+      console.log("location==>",location.href);
+      // ...
+    } else {
+      console.log("user sign out");
+      // User is signed out
+      // ...
+    }
+  });
+
+
+
+
+
+
+
 
 
 const toogleRegister = document.querySelectorAll(".input-register")[0];
