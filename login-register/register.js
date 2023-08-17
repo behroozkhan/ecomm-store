1f366783d270b8cb128703216aa4dec65aa64849
Behrooz Khan <117090048+behroozkhan@users.noreply.github.com>
2023-08-18 03:41:08 +0500

    working on

--- login-register/register.js
+++ login-register/register.js
@@ -1,58 +1,93 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+// import {
+//   getAuth,
+//   createUserWithEmailAndPassword,
+// } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+// import {
+//   getFirestore,
+//   collection,
+//   addDoc,
+// } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAOt0YRqCBI5MvtYzZaJiILrjlsA9vWn6w",
-  authDomain: "signup-login-74d21.firebaseapp.com",
-  projectId: "signup-login-74d21",
-  storageBucket: "signup-login-74d21.appspot.com",
-  messagingSenderId: "167744399342",
-  appId: "1:167744399342:web:50d079ab744413004a90ff",
-  measurementId: "G-PQXQJB56F5",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAOt0YRqCBI5MvtYzZaJiILrjlsA9vWn6w",
+//   authDomain: "signup-login-74d21.firebaseapp.com",
+//   projectId: "signup-login-74d21",
+//   storageBucket: "signup-login-74d21.appspot.com",
+//   messagingSenderId: "167744399342",
+//   appId: "1:167744399342:web:50d079ab744413004a90ff",
+//   measurementId: "G-PQXQJB56F5",
+// };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// const db = getFirestore(app);
+
+// let registerBtn = document.querySelectorAll("#register-button")[0];
+// let register = () => {
+//   let userName = document.querySelectorAll("#username-reg")[0].value;
+//   let email = document.querySelectorAll("#email-reg")[0].value;
+//   let password = document.querySelectorAll("#password-reg")[0].value;
+
+//   let userlistObj = {
+//     userName: userName,
+//     email: email,
+//     password: password,
+//   };
+
+//   createUserWithEmailAndPassword(auth, userlistObj.email, userlistObj.password)
+//     .then(async (userCredential) => {
+//       const user = userCredential.user;
+//       try {
+//         const docRef = await addDoc(collection(db, "users"), {
+//           ...userlistObj,
+//           uid: user.uid,
+//         });
+//         console.log("Document written with ID: ", docRef.id);
+//       } catch (e) {
+//         console.error("Error adding document: ", e);
+//       }
+//     })
+//     .catch((error) => {
+//       const errorCode = error.code;
+//       const errorMessage = error.message;
+//       console.log("errorMessage==>", errorMessage);
+//     });
+// };
+
+
+
+import { auth,createUserWithEmailAndPassword,db,doc,setDoc } from "../firebaseConfig.js";
 
 let registerBtn = document.querySelectorAll("#register-button")[0];
-let register = () => {
+
+const register = () => {
   let userName = document.querySelectorAll("#username-reg")[0].value;
   let email = document.querySelectorAll("#email-reg")[0].value;
   let password = document.querySelectorAll("#password-reg")[0].value;
-
-  let userlistObj = {
-    userName: userName,
-    email: email,
-    password: password,
-  };
-
-  createUserWithEmailAndPassword(auth, userlistObj.email, userlistObj.password)
-    .then(async (userCredential) => {
-      const user = userCredential.user;
-      try {
-        const docRef = await addDoc(collection(db, "users"), {
-          ...userlistObj,
-          uid: user.uid,
-        });
-        console.log("Document written with ID: ", docRef.id);
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
+  
+  createUserWithEmailAndPassword(auth, email, password)
+  .then(async (userCredential) => {
+    const user = userCredential.user;
+    console.log("user-register", user);
+      const userInfo = {
+        userName,
+        email,
+        uid: user.uid,
+      };
+      
+      const ecomUserRef = doc(db, "ecom_user", user.uid);
+      console.log("ecomUserRef",ecomUserRef);
+      await setDoc(ecomUserRef, userInfo);
+      console.log("user Sign Up");
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
-      console.log("errorMessage==>", errorMessage);
+      console.log("error", error);
+      // ..
     });
-};
+  };
+ 
+  registerBtn.addEventListener("click", register);
 
-registerBtn.addEventListener("click", register);
